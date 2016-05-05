baed5c4b22cbde0ba11b86b001650539df65e132
Chonnipa Kittisiriprasert <Nagisama.j@gmail.com>
2016-05-05 21:01:29 +0700

    encrypt password

--- src/main/java/th/in/nagi/swp/model/User.java
+++ src/main/java/th/in/nagi/swp/model/User.java
@@ -1,13 +1,33 @@
 package th.in.nagi.swp.model;
 
+import java.security.MessageDigest;
+
 public class User {
 
 	private String username;
 	private String password;
 
+	private static MessageDigest md;
+
 	public User(String username, String password) {
 		this.username = username;
-		this.password = password;
+		this.password = encrypt(password);
+	}
+
+	private String encrypt(String password) {
+		try {
+			md = MessageDigest.getInstance("MD5");
+			byte[] passwordBytes = password.getBytes();
+			md.reset();
+			byte[] digested = md.digest(passwordBytes);
+			StringBuffer buffer = new StringBuffer();
+			for (int i = 0; i < digested.length; i++) {
+				buffer.append(Integer.toHexString(0xff & digested[i]));
+			}
+			return buffer.toString();
+		} catch (Exception e) {
+			return null;
+		}
 	}
 
 	public String getUsername() {
@@ -15,6 +35,6 @@ public class User {
 	}
 
 	public boolean checkPassword(String guess) {
-		return password.equals(guess);
+		return password.equals(encrypt(guess));
 	}
 }
